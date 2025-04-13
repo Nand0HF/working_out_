edda2e19b28c4bb3a858ba6f1940c6491bb8604c
Nand0HF <nandohdzfundora@gmail.com>
2025-04-13 09:40:36 -0400

    045_leap_year simplified code

--- exercism/045_leap_year/leap.js
+++ exercism/045_leap_year/leap.js
@@ -6,11 +6,7 @@
 export const isLeap = (year) => {
   if (year % 4 === 0) {
     if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        return true;
-      } else {
-        return false;
-      }
+      return year % 400 === 0;
     } else {
       return true;
     }
