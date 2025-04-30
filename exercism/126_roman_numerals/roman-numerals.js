612c72cb370f85005222ceacb8369d370fc65631
Nand0HF <nandohdzfundora@gmail.com>
2025-04-30 15:35:26 -0400

    126_roman_numerals

---
+++ exercism/126_roman_numerals/roman-numerals.js
@@ -0,0 +1,35 @@
+//
+// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const toRoman = (num) => {
+  
+  let romanNum = '';
+
+  const romanNumerals = [
+        { value: 1000, symbol: "M" },
+        { value: 900, symbol: "CM" },
+        { value: 500, symbol: "D" },
+        { value: 400, symbol: "CD" },
+        { value: 100, symbol: "C" },
+        { value: 90, symbol: "XC" },
+        { value: 50, symbol: "L" },
+        { value: 40, symbol: "XL" },
+        { value: 10, symbol: "X" },
+        { value: 9, symbol: "IX" },
+        { value: 5, symbol: "V" },
+        { value: 4, symbol: "IV" },
+        { value: 1, symbol: "I" }
+    ];
+  
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].value) {
+      romanNum += romanNumerals[i].symbol;
+      num -= romanNumerals[i].value;
+    }
+  }
+
+  return romanNum;
+  
+};
