72f5a1646c064506abd9e15c8b2fa1390a180327
Nand0HF <nandohdzfundora@gmail.com>
2025-05-12 16:12:20 -0400

    075_largest_series_product solution update

--- exercism/075_largest_series_product/largest-series-product.js
+++ exercism/075_largest_series_product/largest-series-product.js
@@ -9,45 +9,28 @@ export const largestProduct = (input, span) => {
   }
 
   if (span < 1) {
-    throw new Error('span must be greater than zero');
+    throw new Error('span must be greater than 0');
   }
 
   if (span > input.length) {
     throw new Error('span must be smaller than string length');
   }
 
-  let first, last, output, product, i, input_length, update_pointers;
+  if (!/^[0-9]+$/.test(input)) {
+    throw new Error('digits input must only contain digits');
+  }
 
-  output = 0;
-  i = -1;
-  input_length = input.length;
-  update_pointers = () => {
-    first = ++i;
-    last = first + span - 1;
-    product = 1;
-  };
-  update_pointers();
+  let largestProduct = 0;
 
-  while (last < input_length) {
-
-    if (input[i] < '0' || input[i] > '9') {
-      throw new Error('digits input must only contain digits');
+  for (let i = 0; i <= input.length - span; i++) {
+    let product = 1;
+    for (let j = i; j < i + span; j++) {
+      product *= parseInt(input[j], 10);
     }
-
-    if (input[i] === '0') {
-      update_pointers();
-    }
-
-    else if (i <= last) {
-      product *= +input[i];
-      if (i++ === last && output < product) {
-        output = product;
-      }
-    }
-    else {
-      product /= +input[first++];
-      last++;
+    if (product > largestProduct) {
+      largestProduct = product;
     }
   }
-  return output;
+
+  return largestProduct;
 };
