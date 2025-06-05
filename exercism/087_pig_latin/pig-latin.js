97dba02470da888f8c8d1b804791e47120785286
Nand0HF <nandohdzfundora@gmail.com>
2025-06-05 12:29:56 -0400

    087 code refactoring

--- exercism/087_pig_latin/pig-latin.js
+++ exercism/087_pig_latin/pig-latin.js
@@ -4,69 +4,32 @@
 //
 
 export const translate = (phrase) => {
-  const words = phrase.split(" ");
-  const translatedWords = [];
+  return phrase.split(' ').map(word => translateWord(word)).join(' ');
+};
 
-  for (let word of words) {
-    const originalWord = word;
-    let translatedWord = "";
-
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-
-    const isVowel = (char) => vowels.includes(char);
-
-    if (isVowel(originalWord[0]) || originalWord.startsWith("xr") || originalWord.startsWith("yt")) {
-      translatedWord = originalWord + "ay";
-    }
-    else if (originalWord.includes("qu")) {
-      const quIndex = originalWord.indexOf("qu");
-      const precedingConsonants = originalWord.substring(0, quIndex);
-      const remainder = originalWord.substring(quIndex + 2);
-      translatedWord = remainder + precedingConsonants + "quay";
-    }
-    else if (originalWord.includes("y") && !isVowel(originalWord[0])) {
-      let firstVowelOrYIndex = -1;
-      for (let i = 0; i < originalWord.length; i++) {
-        if (isVowel(originalWord[i]) || (originalWord[i] === 'y' && i > 0)) {
-          firstVowelOrYIndex = i;
-          break;
-        }
-      }
-
-      if (firstVowelOrYIndex !== -1) {
-        const leadingConsonants = originalWord.substring(0, firstVowelOrYIndex);
-        translatedWord = originalWord.substring(firstVowelOrYIndex) + leadingConsonants + "ay";
-      } else {
-        let firstConsonantBlockEnd = 0;
-        while (firstConsonantBlockEnd < originalWord.length && !isVowel(originalWord[firstConsonantBlockEnd]) && originalWord[firstConsonantBlockEnd] !== 'y') {
-          firstConsonantBlockEnd++;
-        }
-        if (firstConsonantBlockEnd === originalWord.length) { 
-            translatedWord = originalWord + "ay";
-        } else {
-            const leadingConsonants = originalWord.substring(0, firstConsonantBlockEnd);
-            translatedWord = originalWord.substring(firstConsonantBlockEnd) + leadingConsonants + "ay";
-        }
-      }
-    }
-    else {
-      let firstVowelIndex = -1;
-      for (let i = 0; i < originalWord.length; i++) {
-        if (isVowel(originalWord[i])) {
-          firstVowelIndex = i;
-          break;
-        }
-      }
-
-      if (firstVowelIndex === -1) {
-        translatedWord = originalWord + "ay";
-      } else {
-        const leadingConsonants = originalWord.substring(0, firstVowelIndex);
-        translatedWord = originalWord.substring(firstVowelIndex) + leadingConsonants + "ay";
-      }
-    }
-    translatedWords.push(translatedWord);
+const translateWord = (word) => {
+  if (word.match(/^(a|e|i|o|u|xr|yt)/)) {
+    return word + 'ay';
   }
 
-  return translatedWords.join(" ");
+  let splitIndex = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    const currentChar = word[i];
+    const prevChar = word[i - 1];
+
+    if (['a', 'e', 'i', 'o', 'u'].includes(currentChar)) {
+      if (currentChar === 'u' && prevChar === 'q') {
+        continue;
+      }
+      splitIndex = i;
+      break;
+    }
+
+    if (currentChar === 'y' && i > 0 && !['a', 'e', 'i', 'o', 'u'].includes(prevChar)) {
+      splitIndex = i;
+      break;
+    }
+  }
+  return word.slice(splitIndex) + word.slice(0, splitIndex) + 'ay';
 };
